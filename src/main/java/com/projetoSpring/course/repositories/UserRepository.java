962c48d5edb9239b48053bfa137952e357160c26
Vinicius <vinirabaneda@hotmail.com>
2024-12-20 16:17:46 -0300

    Service layer, component registration

--- src/main/java/com/projetoSpring/course/repositories/UserRepository.java
+++ src/main/java/com/projetoSpring/course/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.projetoSpring.course.entities.User;
 
+// não precisa adicionar @Repository pois esta herdando da classe JpaRepository que já esta registrado como repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
--- src/main/java/com/projetoSpring/course/resources/UserResource.java
+++ src/main/java/com/projetoSpring/course/resources/UserResource.java
@@ -1,21 +1,34 @@
 package com.projetoSpring.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetoSpring.course.entities.User;
+import com.projetoSpring.course.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/users") // indica o caminho raiz, ou seja, para realizar uma requisição será necessário passar /user
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "maria","maria@gmail.com" , "99999999", "1234");
-		return ResponseEntity.ok().body(u);
+	@Autowired
+	private UserService service;
+
+	@GetMapping // por não termos passado o value, este codigo esta indicando que o caminho é o mesmo do Request Mapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
-	
+	@GetMapping(value = "/{id}") // {} notação usada para definir que esta parte do path é variável.EX: user/1
+	public ResponseEntity<User> findById(@PathVariable Long id){ // @PathVariable é necessário para declarar que o valor da url irá mudar constantemente
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
