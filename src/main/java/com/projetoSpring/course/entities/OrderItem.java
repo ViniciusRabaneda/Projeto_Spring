049eaf32bea32cb7327466050a7309b9b359c8c7
Vinicius <vinirabaneda@hotmail.com>
2024-12-23 15:14:17 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/projetoSpring/course/entities/OrderItem.java
@@ -0,0 +1,93 @@
+package com.projetoSpring.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.projetoSpring.course.entities.pk.OrderItemPk;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId  // indica que fara uso da classe da chave composta
+	private OrderItemPk id = new OrderItemPk(); // sempre que criar chave composta precisa instanciar
+
+	private Integer quantity;
+	private Double price;
+
+	public OrderItem() {
+
+	}
+	
+	
+	//o construtor é diferente neste caso pois simula uma chave composta
+	public OrderItem(Order order, Product product, Integer quantity, Double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quantity = quantity;
+		this.price = price;
+	}
+	
+	
+	//métodos feitos manualmente por conta da chave composta -- Inicio --
+	
+	@JsonIgnore // para "cortar" a associação de mão dupla e cair em looping
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct(product);
+	}
+	
+	// -- Fim --
+	
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+}
