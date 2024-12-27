84ca1568088e6fa66fa81209b604e9329459bc0d
Vinicius <vinirabaneda@hotmail.com>
2024-12-26 22:14:16 -0300

    User Delete

--- src/main/java/com/projetoSpring/course/services/UserService.java
+++ src/main/java/com/projetoSpring/course/services/UserService.java
@@ -11,22 +11,26 @@ import com.projetoSpring.course.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repository.findAll();
-		
+
 	}
-	
+
 	public User findById(Long Id) {
-		Optional<User> obj =  repository.findById(Id);
-		return obj.get(); // Foi necessário utilizar o get pois estamos usando o Optional, se não utilizassemos e o valor voltasse null daria problema
+		Optional<User> obj = repository.findById(Id);
+		return obj.get(); // Foi necessário utilizar o get pois estamos usando o Optional, se não
+							// utilizassemos e o valor voltasse null daria problema
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
